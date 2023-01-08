aad55515eb2b6658495404616c683c69fe18afaa
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-01-08 01:28:05 -0800

    feature/functionalTableOfContents

---
+++ README.md
@@ -0,0 +1,34 @@
+# Readme-Generator-SN286-8nc-
+  ![GitHub License](https://img.shields.io/badge/license-MIT-lightblue.svg)
+  ## Description
+      A basic program that accepts user input to dynamically create a README.md file.
+  ## Table of Contents
+  [Description](#description)
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [License](#license)
+
+  [How to Contribute](#how-to-contribute)
+
+  [Tests](#tests)
+
+  [Questions](#questions)
+
+  ## Installation
+      Clone the GitHub project and run npm install.
+  ## Usage
+      Follow the prompts after running node index.js. This very README is an example result!
+  ## License
+![GitHub License](https://img.shields.io/badge/license-MIT-lightblue.svg)
+
+Link to license: [License](#MIT)
+  ## How to Contribute
+      Contact me with any bugfixes.
+  ## Tests
+      Run the program and test it for weaknesses.
+  ## Questions?
+Access my GitHub at: https://github.com/BaldurTheBuilder, or email me at: jack.kokcarlson@gmail.com
+      
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,5 @@
 //WHEN I choose a license for my application from a list of options
-//THEN a badge for that license is added near the top of the README and a notice is added to the section of
-  //the README entitled License that explains which license the application is covered under
+//THEN a badge for that license is added near the top of the README and a notice is added to the license section noting what license is being used.
 function renderLicenseBadge(license) {
   if(license && license.toLowerCase() !== 'none') {
     return `![GitHub License](https://img.shields.io/badge/license-${license}-lightblue.svg)`;
@@ -17,7 +16,7 @@ function renderLicenseLink(license) {
 
 function renderLicenseSection(license) {
   if(license && license.toLowerCase() !== 'none') {
-   return `## License\n${renderLicenseBadge(license)}\n\n${renderLicenseLink(license)}`;
+   return `\n${renderLicenseBadge(license)}\n\nLink to license: ${renderLicenseLink(license)}`;
   }
   else return '';
 }
@@ -26,30 +25,36 @@ function renderLicenseSection(license) {
 //WHEN I enter my email address it is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 function renderQuestionsSection(username, email) {
   if(username && email) {
-    return `##Questions?\nAccess my GitHub at: https://github.com/${username}, or email me at: ${email}`;
+    return `## Questions?\nAccess my GitHub at: https://github.com/${username}, or email me at: ${email}`;
   }
   else if(username) {
-    return `##Questions?\nAccess my GitHub at: https://github.com/${username}`;
+    return `## Questions?\nAccess my GitHub at: https://github.com/${username}`;
   }
   else if(email){
-    return `##Questions?\nEmail me at: ${email}`;
+    return `## Questions?\nEmail me at: ${email}`;
   }
-  else return '';
-
+  else return '## Questions?\nBest of luck!';
 }
 
+//WHEN I click on the links in the Table of Contents I am taken to the corresponding section of the README
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
   ## Description
       ${data.description}
   ## Table of Contents
-      ${data.tableOfContents}
+  [Description](#description)\n
+  [Installation](#installation)\n
+  [Usage](#usage)\n
+  [License](#license)\n
+  [How to Contribute](#how-to-contribute)\n
+  [Tests](#tests)\n
+  [Questions](#questions)\n
   ## Installation
       ${data.installation}
   ## Usage
       ${data.usage}
-  ${renderLicenseSection(data.license)}
+  ## License${renderLicenseSection(data.license)}
   ## How to Contribute
       ${data.contributing}
   ## Tests
